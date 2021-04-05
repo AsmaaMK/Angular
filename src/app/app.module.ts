af9c59d871c0e6bd137f30659f52c77433263314
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2021-04-05 22:22:07 +0200

    Add product-box component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductBoxComponent } from './product-box/product-box.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductBoxComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-box/product-box.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+
+@Component({
+  selector: 'app-product-box',
+  templateUrl: './product-box.component.html',
+  styleUrls: ['./product-box.component.css']
+})
+export class ProductBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    $(document).ready(function () {
+      let body = <HTMLDivElement>document.body;
+      let script = document.createElement('script');
+      script.innerHTML = '';
+      script.src = '../../assets/js/script.js';
+      script.async = true;
+      script.defer = true;
+      body.appendChild(script);
+    });
+  }
+
+}
