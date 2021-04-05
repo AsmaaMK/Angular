005eeb00191082d3763fd5671891b0c097161e3f
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2021-04-05 22:30:31 +0200

    Add header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    $(document).ready(function()
+    {
+      let body = <HTMLDivElement> document.body;
+      let script = document.createElement('script');
+      script.innerHTML = '';
+      script.src = '../assets/js/script.js';
+      script.async = true;
+      script.defer = true;
+      body.appendChild(script);
+    });
+  }
+
+}
