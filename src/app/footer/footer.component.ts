67a4988c08fffa20183d935a107a9253d3c5d2c2
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2021-04-05 22:43:54 +0200

    Add footer component

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    $(document).ready(function()
+    {
+      let body = <HTMLDivElement> document.body;
+      let script = document.createElement('script');
+      script.innerHTML = '';
+      script.src = '../../assets/js/script.js';
+      script.async = true;
+      script.defer = true;
+      body.appendChild(script);
+    });
+  }
+
+}
